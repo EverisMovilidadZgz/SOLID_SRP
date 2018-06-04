b5e1549d7a88a65c9c3631ba659866a24b547b96
Víctor Manuel Cañada Ojeda <victorcanoje@gmail.com>
2018-06-04 12:25:54 +0200

    LO HEMOS SOLUCIONADO CREANDO UNA CLASE EN LA QUE DELEGAR LAS POSIBLES FORMAS DE

--- app/src/main/java/victordev/es/solidsrp/RegisterStatus.java
+++ app/src/main/java/victordev/es/solidsrp/RegisterStatus.java
@@ -1,5 +1,27 @@
 package victordev.es.solidsrp;
 
 public class RegisterStatus {
-    //TODO clase con la lógica del status de un registro
+    //ENUMERADO PARA INDICAR LAS POSIBILIDADES A LA HORA DE MOSTRAR EL STADO DEL LOGIN
+    enum ShowStatus {TERMINAL, TOAST}
+
+    public static void showRegisterStatus(RegisterStatus status, ShowStatus type) {
+        switch (type) {
+            case TERMINAL:
+                showRegisterStatusInTerminal(status);
+                break;
+            case  TOAST:
+                showRegisterStatusWithToast(status);
+                break;
+        }
+    }
+
+    private static void showRegisterStatusInTerminal(RegisterStatus status) {
+        //TODO clase con la lógica del status de un registro por terminal
+
+    }
+
+    private static void showRegisterStatusWithToast(RegisterStatus status) {
+        //TODO clase con la lógica del status de un registro con un Toast
+
+    }
 }
--- app/src/main/java/victordev/es/solidsrp/UserRegister.java
+++ app/src/main/java/victordev/es/solidsrp/UserRegister.java
@@ -1,7 +1,5 @@
 package victordev.es.solidsrp;
 
-import android.util.Log;
-
 /**
  * ROMPLE EL SRP PORQUE ADEMÁS DE LA LÓGICA NECESARIA PARA REGISTRAR UN USUARIO
  * TIENE LA LÓGICA DE MOSTRAR EL RESULTADO DEL REGISTRO. ACTUALMENTE LO HACE POR
@@ -11,25 +9,23 @@ import android.util.Log;
  * UN CAMBIO EN LA FORMA DE MOSTRAR EL RESULTADO DEL REGISTRO IMPLICARÍA UN CAMBIO
  * EN LA CLASE
  *
+ * LO HEMOS SOLUCIONADO CREANDO UNA CLASE EN LA QUE DELEGAR LAS POSIBLES FORMAS DE
+ * MOSTRAR EL RESULTADO. SI EN UN FUTURO EXISTE O CAMBIA ALGUNA DE LAS MANERAS DE
+ * MOSTRAR EL RESULTADO, SÓLO HEMOS DE MOFIDICAR ESA CLASE.
+ *
  */
 public class UserRegister {
 
-    public static void register(User user) throws UserRegisterException {
-            if (user != null) {
-                showRegisterStatus(callToRegisterProcess(user)); //Muestra el estado del registro.
-            } else {
-                throw new UserRegisterException();
-            }
+    public static void register(User user, RegisterStatus.ShowStatus type) throws UserRegisterException {
+        if (user != null) {
+            RegisterStatus.showRegisterStatus(callToRegisterProcess(user), type);
+        } else {
+            throw new UserRegisterException();
+        }
     }
 
     private static RegisterStatus callToRegisterProcess(User user) {
-        //TODO LÓGICA NECESARIA PARA LA LLAMADA AL SERVICIO
+        //TODO LÓGICA NECESARIA PARA LA LLAMADA AL SERVICIO QUE NOS DEVOLVERÁ UN ESTADO DE REGISTRO
         return new RegisterStatus();
     }
-
-    private static void showRegisterStatus(RegisterStatus status) {
-        Log.d("REGISTER_STATUS", status.toString());
-    }
 }
-
-
